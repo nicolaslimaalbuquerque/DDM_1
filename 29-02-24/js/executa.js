a296140276f3c1e85ca33bfe0f1aa6720672462c
Nicolas Lima Albuquerque <124801443+nicolaslimaalbuquerque@users.noreply.github.com>
2024-02-29 11:08:22 -0300

    Add files via upload

---
+++ 29-02-24/js/executa.js
@@ -0,0 +1,9 @@
+var resultado = document.getElementById('resultado')
+
+function pegaNome(){
+    var nome = document.getElementById('nome').value 
+    //console.log('Nome: ', nome)
+    resultado.innerHTML = nome
+
+
+}
